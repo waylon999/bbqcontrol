d3cc941adca2ae8b64688dabbaf2b54ea1ac8197
Bryan <bryanwyman@gmail.com>
2013-09-01 11:45:49 -0700

    Add angular-cookies to the project

--- bbq/static/js/app.js
+++ bbq/static/js/app.js
@@ -1,11 +1,15 @@
 'use strict';
 
 // Declare app level module which depends on filters, and services
-var BBQApp = angular.module('bbqapp', []);
+var BBQApp = angular.module('bbqapp', ['ngCookies']);
+
+BBQApp.run(function ($http, $cookies) {
+  $http.defaults.headers.common['X-CSRFToken'] = $cookies['csrftoken'];
+});
 
 BBQApp.config(function($routeProvider) {
   $routeProvider.when('/home', {
-    templateUrl: 'static/partials/home.html',
+    templateUrl: '/static/partials/home.html',
     resolve: {
       data: function () {
       }
